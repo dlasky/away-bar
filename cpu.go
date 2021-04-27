d30b586ee2ab757fbe8b3b0709eb04ea3993834e
Dan <github@aerolith.net>
2021-04-27 19:41:10 -0400

    missing error

--- cpu.go
+++ cpu.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/shirou/gopsutil/v3/cpu"
-	"log"
-	"time"
 )
 
 func InitCPU() (gtk.IWidget, error) {
@@ -19,6 +20,9 @@ func InitCPU() (gtk.IWidget, error) {
 	go func() {
 		for {
 			c, err := cpu.Percent(5*time.Second, false)
+			if err != nil {
+				log.Fatal("cpu fetch error")
+			}
 
 			s := fmt.Sprintf("cpu: %.0f %%", c[0])
 			_, err = glib.IdleAdd(cpuLabel.SetText, s)
