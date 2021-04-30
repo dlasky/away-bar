f0d9a2e7a44c05daed79177b46facfd2eaec2334
Dan <github@aerolith.net>
2021-04-30 18:25:14 -0400

    bl wip

--- backlight.go
+++ backlight.go
@@ -1,1 +1,11 @@
 package main
+
+import "github.com/gotk3/gotk3/gtk"
+
+func InitBacklight() (gtk.IWidget, error) {
+	mod, err := NewModule("backlight", "", "")
+	if err != nil {
+		return nil, err
+	}
+	return mod.box, nil
+}
