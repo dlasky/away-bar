cabadbf3fbc612b998138377eda4a7f1c4ef279d
Dan <github@aerolith.net>
2023-12-18 23:30:17 -0500

    finish dyn

--- icon/dynamic.go
+++ icon/dynamic.go
@@ -1,6 +1,8 @@
 package icon
 
-type DynamicIconMap = map[int]string
+import "github.com/gotk3/gotk3/glib"
+
+type DynamicIconMap = map[string][]int
 
 func NewDynamicIcon(config DynamicIconMap) (*Icon, error) {
 	icon, err := getIconBase(DynamicIcon, "")
@@ -8,6 +10,15 @@ func NewDynamicIcon(config DynamicIconMap) (*Icon, error) {
 	return icon, err
 }
 
-func (i *Icon) Render(Value any) {
-
+func (i *Icon) Render(value int) {
+	//TODO: replace with an interval tree
+	var candidate string
+	for path, interval := range *i.config {
+		if value > interval[0] && value < interval[1] {
+			candidate = path
+		}
+	}
+	glib.IdleAdd(func() {
+		i.icon.SetFromFile(candidate)
+	})
 }
